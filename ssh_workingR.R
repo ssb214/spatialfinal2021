c4f3fac43801b170897a447a1727238a92b2d130
Hoffman <SSBUCKE@emory.edu>
2021-11-15 10:56:25 -0500

    Adding packages and data calls

--- ssh_workingR.R
+++ ssh_workingR.R
@@ -1,2 +1,21 @@
 # Susan's Working File 
 
+# Packages 
+
+library(tidyverse)
+library(raster)
+library(rgdal)
+library(sf)
+library(tmap)
+library(readxl)
+
+# Data
+
+## outcome data
+EJscreen <- read.csv("Data/ej_ga.csv")
+
+## exposure geometry data 
+HOLC_map <- readOGR(dsn=path.expand("Data/HRS2020-Shapefiles/HRS2020"),
+                    layer="HRS2020")
+## exposure attribute data
+HOLC_score <- read_excel("Data/Historic Redlining Score 2020.xlsx")
